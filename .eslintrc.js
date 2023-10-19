dac57994bd35264db604d562d7f9003fefcf974b
Kuntey <70432310+Kuntey@users.noreply.github.com>
2023-10-19 08:06:50 +0000

    移除prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,7 +26,6 @@ module.exports = {
         'plugin:import/recommended',
         'plugin:import/typescript',
         'plugin:vue/vue3-recommended',
-        'plugin:prettier/recommended',
         './.eslintrc-auto-import.json',
     ],
     settings: {
@@ -37,7 +36,6 @@ module.exports = {
         },
     },
     rules: {
-        'prettier/prettier': 0,
         // Vue: Recommended rules to be closed or modify
         'vue/require-default-prop': 0,
         'vue/singleline-html-element-content-newline': 0,
