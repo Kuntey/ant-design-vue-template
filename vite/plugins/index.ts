db452f948734246185abbce5c42f889fecff87ad
Kuntey <70432310+Kuntey@users.noreply.github.com>
2023-10-25 08:55:48 +0000

    feat(custom): 添加vue-devtools插件

--- vite/plugins/index.ts
+++ vite/plugins/index.ts
@@ -1,13 +1,14 @@
-import vue from "@vitejs/plugin-vue";
+import vue from '@vitejs/plugin-vue';
 
-import createAutoImport from "./auto-import";
-import createUnocss from "./unocss";
-import createCompression from "./compression";
-import createVueComponents from "./vue-components";
-import createVueI18n from "./vue-il8n";
+import createVueDevTools from 'vite-plugin-vue-devtools';
+import createAutoImport from './auto-import';
+import createUnocss from './unocss';
+import createCompression from './compression';
+import createVueComponents from './vue-components';
+import createVueI18n from './vue-il8n';
 
 export default function createVitePlugins(viteEnv: any, isBuild = false) {
-  const vitePlugins = [vue(), createUnocss()];
+  const vitePlugins = [createVueDevTools(), vue(), createUnocss()];
   vitePlugins.push(createAutoImport());
   vitePlugins.push(createVueComponents());
   vitePlugins.push(createVueI18n());
